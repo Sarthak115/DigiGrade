16e32b655c68a7585f27d64bc7d89ad47e0f3ef9
sarthak115 <supersarthak8@gmail.com>
2025-11-10 13:18:11 +0530

    database

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const url = process.env.MONGO_URI || "mongodb+srv://andumandu6:andumandu6@cluster0.eh098m7.mongodb.net/"
+    const url = process.env.MONGO_URI
     const conn = await mongoose.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
